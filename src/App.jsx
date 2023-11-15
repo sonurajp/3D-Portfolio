c3325b51fd9218ac56bc771bde9e8fa7c6e22579
sonurajp <sonurajp49@gmail.com>
2023-11-15 22:05:46 +0530

    add pages files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
-import "./index.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/*"
+            element={
+              <>
+                <Routes>
+                  <Route path="/about" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
