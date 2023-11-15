4974db3aea18f13917ed7e136e3243fa28a6a1e9
sonurajp <sonurajp49@gmail.com>
2023-11-16 01:03:41 +0530

    install required packages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+
+// import { logo } from "../assets/images";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white flex items-center justify-center  font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">SR</p>
+        {/* <img src={logo} alt='logo' className='w-18 h-18 object-contain' /> */}
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
