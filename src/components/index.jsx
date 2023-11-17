171cbc6e07fe6828c29260b00d8806617a762dda
sonurajp <sonurajp49@gmail.com>
2023-11-18 00:09:34 +0530

    add homeinfo and loader

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import HomeInfo from "./HomeInfo";
+
+export { Loader, Navbar, Footer, HomeInfo };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense, useEffect, useRef, useState } from "react";
 
 import sakura from "../assets/sakura.mp3";
-// import { HomeInfo, Loader } from "../components";
+import { HomeInfo, Loader } from "../components";
 import { soundoff, soundon } from "../assets/icons";
 import { Island } from "../models";
 
@@ -60,7 +60,7 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        {/* {currentStage && <HomeInfo currentStage={currentStage} />} */}
+        {currentStage && <HomeInfo currentStage={currentStage} />}
       </div>
 
       <Canvas
